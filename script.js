5f02b193330dc09d1d1fbd60c24bcfbc331f260a
jigen3971 <jigen3971@gmail.com>
2026-05-24 14:32:25 +0900

    変更

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const chordNames = ["c", "d", "e", "f", "g", "a", "b"];
-
 function selectChord(chord) {
   const subButtons = document.getElementById("subButtons");
 
@@ -13,15 +11,12 @@ function selectChord(chord) {
 }
 
 function showChord(folder, file) {
-  document.getElementById("chordImage").src =
-    `images/${folder}/${file}`;
+  document.getElementById("chordImage").src = `images/${folder}/${file}`;
 }
 
 function startVoice() {
-
   const SpeechRecognition =
-    window.SpeechRecognition ||
-    window.webkitSpeechRecognition;
+    window.SpeechRecognition || window.webkitSpeechRecognition;
 
   if (!SpeechRecognition) {
     alert("音声認識に対応していません");
@@ -29,7 +24,6 @@ function startVoice() {
   }
 
   const recognition = new SpeechRecognition();
-
   recognition.lang = "ja-JP";
   recognition.interimResults = false;
   recognition.continuous = false;
@@ -37,101 +31,47 @@ function startVoice() {
   recognition.start();
 
   recognition.onresult = function(event) {
-
-    let text =
-      event.results[0][0].transcript.toLowerCase();
-
+    let text = event.results[0][0].transcript.toLowerCase();
     text = text.replace(/\s/g, "");
 
-    console.log(text);
+    let chord = "";
 
-    // C
-    if (text.includes("c7") || text.includes("シーセブン")) {
-      showChord("c", "c7.jpg");
-    }
-    else if (text.includes("cm") || text.includes("シーマイナー")) {
-      showChord("c", "cm.jpg");
-    }
-    else if (text.includes("c") || text.includes("シー")) {
-      selectChord("c");
-    }
+    if (text.includes("シー") || text.includes("c")) chord = "c";
+    else if (text.includes("ディー") || text.includes("d")) chord = "d";
+    else if (text.includes("イー") || text.includes("e")) chord = "e";
+    else if (text.includes("エフ") || text.includes("f")) chord = "f";
+    else if (text.includes("ジー") || text.includes("g")) chord = "g";
+    else if (text.includes("エー") || text.includes("a")) chord = "a";
+    else if (text.includes("ビー") || text.includes("b")) chord = "b";
 
-    // D
-    else if (text.includes("d7") || text.includes("ディーセブン")) {
-      showChord("d", "d7.jpg");
-    }
-    else if (text.includes("dm") || text.includes("ディーマイナー")) {
-      showChord("d", "dm.jpg");
-    }
-    else if (text.includes("d") || text.includes("ディー")) {
-      selectChord("d");
-    }
-
-    // E
-    else if (text.includes("e7") || text.includes("イーセブン")) {
-      showChord("e", "e7.jpg");
-    }
-    else if (text.includes("em") || text.includes("イーマイナー")) {
-      showChord("e", "em.jpg");
-    }
-    else if (text.includes("e") || text.includes("イー")) {
-      selectChord("e");
-    }
-
-    // F
-    else if (text.includes("f7") || text.includes("エフセブン")) {
-      showChord("f", "f7.jpg");
-    }
-    else if (text.includes("fm") || text.includes("エフマイナー")) {
-      showChord("f", "fm.jpg");
-    }
-    else if (text.includes("f") || text.includes("エフ")) {
-      selectChord("f");
-    }
-
-    // G
-    else if (text.includes("g7") || text.includes("ジーセブン")) {
-      showChord("g", "g7.jpg");
-    }
-    else if (text.includes("gm") || text.includes("ジーマイナー")) {
-      showChord("g", "gm.jpg");
-    }
-    else if (text.includes("g") || text.includes("ジー")) {
-      selectChord("g");
-    }
-
-    // A
-    else if (text.includes("a7") || text.includes("エーセブン")) {
-      showChord("a", "a7.jpg");
-    }
-    else if (text.includes("am") || text.includes("エーマイナー")) {
-      showChord("a", "am.jpg");
-    }
-    else if (text.includes("a") || text.includes("エー")) {
-      selectChord("a");
-    }
-
-    // B
-    else if (text.includes("b7") || text.includes("ビーセブン")) {
-      showChord("b", "b7.jpg");
-    }
-    else if (text.includes("bm") || text.includes("ビーマイナー")) {
-      showChord("b", "bm.jpg");
-    }
-    else if (text.includes("b") || text.includes("ビー")) {
-      selectChord("b");
-    }
-
-    else {
+    if (!chord) {
       alert("認識できませんでした：" + text);
+      return;
     }
 
-  };
+    selectChord(chord);
 
+    if (
+      text.includes("セブン") ||
+      text.includes("セブンス") ||
+      text.includes("なな") ||
+      text.includes("7")
+    ) {
+      showChord(chord, `${chord}7.jpg`);
+    }
+    else if (
+      text.includes("マイナー") ||
+      text.includes("minor") ||
+      text.includes("m")
+    ) {
+      showChord(chord, `${chord}m.jpg`);
+    }
+    else {
+      showChord(chord, `${chord}.jpg`);
+    }
+  };
 }
 
-document
-  .getElementById("voiceBtn")
-  .addEventListener("click", startVoice);
+document.getElementById("voiceBtn").addEventListener("click", startVoice);
 
 selectChord("c");
